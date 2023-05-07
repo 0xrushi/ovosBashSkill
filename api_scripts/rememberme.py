05f489c1bf23d05169eae90c4d2eeda1f41b94b8
rushic24 <rushic24@gmail.com>
2023-05-06 22:59:34 -0500

    asddsa

--- api_scripts/rememberme.py
+++ api_scripts/rememberme.py
@@ -17,11 +17,11 @@ def recall_stuff(st: str):
     encoded_query = urllib.parse.quote(st)
     try:
         r = requests.get(f"{url}?query={encoded_query}")
-        if r.status_code == 200:
+        if r["status_code"] == 200:
             if r.json()['content'] == "null" or r.json()['content'] == None:
                 return "I dont know"
             return r.json()['content']
         else:
             return "something wrong in the API server"
     except:
-        return "something wrong in the API server"
+        return "something wrong in the API server2"
